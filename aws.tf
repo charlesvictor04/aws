a5d01dae790ba86e2e51d259734ef0e2005f82f1
charlesvictor04 <89581941+charlesvictor04@users.noreply.github.com>
2022-09-21 22:45:09 +0100

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,16 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-00785f4835c6acf64"
   instance_type = "t3.micro"
-  user_data = << EOF
-		#! /bin/bash
-    sudo yum update
-		sudo yum install -y apache2
-		sudo systemctl start apache2
-		sudo systemctl enable apache2
-		echo "Deployed via Terraform"
-	EOF
-
   tags = {
-    Name = "aws-via-jenkins-pr"
+    Name = "Updated-jenkins"
   }
 }
